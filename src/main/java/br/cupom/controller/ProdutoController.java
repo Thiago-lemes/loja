d4f9defd8268613684cabafbb6cae088c5f4fa2c
thiago lemes <Throdrigo28@Hotmail.com>
2024-11-26 18:01:57 -0300

    criacao dos metodos get para o buscar o produto

--- src/main/java/br/cupom/controller/ProdutoController.java
+++ src/main/java/br/cupom/controller/ProdutoController.java
@@ -3,6 +3,10 @@ package br.cupom.controller;
 import br.cupom.dto.ProdutoDTO;
 import br.cupom.entity.ProdutoEntity;
 import br.cupom.service.ProdutoService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,29 +15,46 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/produto")
 public class ProdutoController {
 
-    private  ProdutoService service;
+    private final ProdutoService service;
 
-    public ProdutoController(ProdutoService produtoService) {
-        this.service = produtoService;
+    public ProdutoController(ProdutoService service) {
+        this.service = service;
     }
 
-    @PostMapping("/cadastra")
+    @PostMapping
     public ResponseEntity<ProdutoEntity> cadastrar(@RequestBody ProdutoDTO dto) {
         ProdutoEntity produtoCadastrado = service.cadastra(dto);
-        return ResponseEntity.ok(produtoCadastrado);
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtoCadastrado);
     }
 
     @PutMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public ProdutoEntity atualizarProduto(@PathVariable Long id, @RequestBody ProdutoDTO dto) {
-
-        return service.atualizar(dto, id);
+    public ResponseEntity<ProdutoEntity> atualizarProduto(@PathVariable Long id, @RequestBody ProdutoDTO dto) {
+        ProdutoEntity produtoAtualizado = service.atualizar(dto, id);
+        return ResponseEntity.ok(produtoAtualizado);
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoEntity> buscaProdutoPeloId(@PathVariable Long id) {
+        ProdutoEntity produto = service.buscaProdutoPeloId(id);
+        return ResponseEntity.ok(produto);
+    }
+
+    @GetMapping("/findByAll")
+    public ResponseEntity<Page<ProdutoEntity>> buscaTodosProdutos(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sort) {
+        if (size < 1 || size > 100) {
+            throw new IllegalArgumentException("O tamanho da página deve estar entre 1 e 100.");
+        }
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sort).ascending());
+        Page<ProdutoEntity> produtos = service.buscaTodosProdutos(pageable);
+        return ResponseEntity.ok(produtos);
     }
 }
--- src/main/java/br/cupom/service/ProdutoService.java
+++ src/main/java/br/cupom/service/ProdutoService.java
@@ -3,7 +3,12 @@ package br.cupom.service;
 import br.cupom.dto.ProdutoDTO;
 import br.cupom.entity.ProdutoEntity;
 import br.cupom.repository.ProdutoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
@@ -13,12 +18,14 @@ public class ProdutoService {
         this.repository = repository;
     }
 
+    @Transactional
     public ProdutoEntity cadastra(ProdutoDTO dto) {
         validaProduto(dto);
         ProdutoEntity produto = dto.toEntity();
         return repository.save(produto);
     }
 
+    @Transactional
     public ProdutoEntity atualizar(ProdutoDTO dto, Long id) {
         validaProduto(dto);
         ProdutoEntity produto = dto.toEntity();
@@ -29,12 +36,21 @@ public class ProdutoService {
         return repository.save(produto);
     }
 
-    public void delete(Long id) {
-        ProdutoEntity produto = repository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Produto não encontrado"));
-        repository.delete(produto);
+    public ProdutoEntity buscaProdutoPeloId(Long id) {
+        return repository.findById(id).orElseThrow(() -> new IllegalArgumentException("Cupom não encontrado"));
     }
 
+    public Page<ProdutoEntity> buscaTodosProdutos(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new IllegalArgumentException("Produto com ID " + id + " não encontrado.");
+        }
+        repository.deleteById(id);
+    }
     private void validaProduto(ProdutoDTO dto) {
         if (dto.nome() == null || dto.nome().isEmpty()) {
             throw new IllegalArgumentException("Nome do Produto não pode ser vazio");
@@ -49,9 +65,9 @@ public class ProdutoService {
             throw new IllegalArgumentException("O preço deve ser maior ou igual a zero.");
         }
 
-        repository.findByNome(dto.nome())
-                .ifPresent(produto -> {
-                    throw new IllegalArgumentException("Já existe um produto cadastrado com este nome.");
-                });
+        Optional<ProdutoEntity> existente = repository.findByNome(dto.nome());
+        if (existente.isPresent()) {
+            throw new IllegalArgumentException("Já existe um produto cadastrado com este nome.");
+        }
     }
 }
